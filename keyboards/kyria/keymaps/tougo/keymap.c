70a8391e2d443f76826ad5e7fb704f47a6e14278
wlselastic <s.kouroupis@gmail.com>
2020-12-14 21:49:04 +0000

    update Kyria mappings

--- keyboards/kyria/keymaps/tougo/keymap.c
+++ keyboards/kyria/keymaps/tougo/keymap.c
@@ -14,6 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+#include <stdio.h>
 
 enum layers {
     _QWERTY = 0,
@@ -22,6 +23,11 @@ enum layers {
     _ADJUST
 };
 
+char wpm_str[10];
+//char cckey_str[20];
+//char cckey_buffer[10];
+int position = 0;
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*
  * Base Layer: QWERTY
@@ -29,19 +35,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                              ,-------------------------------------------.
  * |   ESC|   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  |  | \   |
  * |------+------+------+------+------+------|                              |------+------+------+------+------+--------|
- * |  mo3 |   A  |   S  |  D   |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  ' "   |
+ * |  mo3 |   A  |   S  |   D  |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  ' "   |
  * |------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * | Ctrl |   Z  |   X  |   C  |   V  |   B  |LShift| Space|  | Space|RShift|   N  |   M  | ,  < | . >  | /  ? |  sfen  |
- * `--------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                      | Alt  | Tab  | mo2  |LShift| Space|  | Space|RShift| mo1  | lgui | bsp  |
- *                      |      |      |      |      |      |  |      |      |      |      |      |
+ * | Ctrl |   Z  |   X  |   C  |   V  |   B  |      |      |  |      |      |   N  |   M  | ,  < | . >  | /  ? |  sfent |
+ * `--------------------+------+------+------+      +      |  |      +      +------+------+------+----------------------'
+ *                      | Alt  | Tab  | mo2  |LShift| Space|  | Space|RShift| mo1  | lguit| bsp  |
+ *                      |      |      |      |      |      |  |      |      |      | (bsp)|      |
  *                      `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
       KC_ESC,  KC_Q,   KC_W,   KC_E,    KC_R,    KC_T,                                          KC_Y,   KC_U,    KC_I,    KC_O,    KC_P,    KC_PIPE,
       MO(3),   KC_A,   KC_S,   KC_D,    KC_F,    KC_G,                                          KC_H,   KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
       KC_LCTRL,KC_Z,   KC_X,   KC_C,    KC_V,    KC_B,  KC_LSFT, KC_SPACE, KC_SPACE, KC_RSFT, KC_N,   KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT,
-                               KC_LALT, KC_TAB,  MO(2), KC_LSFT, KC_SPACE, KC_SPACE, KC_RSFT, MO(1),  KC_LGUI, KC_BSPACE
+                               KC_LALT, KC_TAB,  MO(2), KC_LSFT, KC_SPACE, KC_SPACE, KC_RSFT, MO(1),  LGUI_T(KC_BSPACE), KC_BSPACE
     ),
 /*
  * Lower Layer: Symbols
@@ -52,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |        |   {  |   }  |  (   |  )   |  `   |                              |   +  |  -   |  =   |  _   |  %   |   +    |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * |        |  |   |  ^   |  [   |  ]   |  ~   |      |      |  |      |      |   &  |  =   |  ,   |  .   |  \   | - _    |
- * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
+ * `----------------------+------+------+------+      +      |  |      +      +------+------+------+----------------------'
  *                        |      |      |      |  ;   |  =   |  |  =   |  ;   |      |      |      |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
@@ -130,22 +136,82 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // }
 
 #ifdef OLED_DRIVER_ENABLE
-static void render_kyria_logo(void) {
-    static const char PROGMEM kyria_logo[] = {
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,128,128,192,224,240,112,120, 56, 60, 28, 30, 14, 14, 14,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7,  7, 14, 14, 14, 30, 28, 60, 56,120,112,240,224,192,128,128,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,  0,  0,  0,192,224,240,124, 62, 31, 15,  7,  3,  1,128,192,224,240,120, 56, 60, 28, 30, 14, 14,  7,  7,135,231,127, 31,255,255, 31,127,231,135,  7,  7, 14, 14, 30, 28, 60, 56,120,240,224,192,128,  1,  3,  7, 15, 31, 62,124,240,224,192,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,240,252,255, 31,  7,  1,  0,  0,192,240,252,254,255,247,243,177,176, 48, 48, 48, 48, 48, 48, 48,120,254,135,  1,  0,  0,255,255,  0,  0,  1,135,254,120, 48, 48, 48, 48, 48, 48, 48,176,177,243,247,255,254,252,240,192,  0,  0,  1,  7, 31,255,252,240,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,255,255,255,  0,  0,  0,  0,  0,254,255,255,  1,  1,  7, 30,120,225,129,131,131,134,134,140,140,152,152,177,183,254,248,224,255,255,224,248,254,183,177,152,152,140,140,134,134,131,131,129,225,120, 30,  7,  1,  1,255,255,254,  0,  0,  0,  0,  0,255,255,255,  0,  0,  0,  0,255,255,  0,  0,192,192, 48, 48,  0,  0,240,240,  0,  0,  0,  0,  0,  0,240,240,  0,  0,240,240,192,192, 48, 48, 48, 48,192,192,  0,  0, 48, 48,243,243,  0,  0,  0,  0,  0,  0, 48, 48, 48, 48, 48, 48,192,192,  0,  0,  0,  0,  0,
-        0,  0,  0,255,255,255,  0,  0,  0,  0,  0,127,255,255,128,128,224,120, 30,135,129,193,193, 97, 97, 49, 49, 25, 25,141,237,127, 31,  7,255,255,  7, 31,127,237,141, 25, 25, 49, 49, 97, 97,193,193,129,135, 30,120,224,128,128,255,255,127,  0,  0,  0,  0,  0,255,255,255,  0,  0,  0,  0, 63, 63,  3,  3, 12, 12, 48, 48,  0,  0,  0,  0, 51, 51, 51, 51, 51, 51, 15, 15,  0,  0, 63, 63,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, 48, 48, 63, 63, 48, 48,  0,  0, 12, 12, 51, 51, 51, 51, 51, 51, 63, 63,  0,  0,  0,  0,  0,
-        0,  0,  0,  0, 15, 63,255,248,224,128,  0,  0,  3, 15, 63,127,255,239,207,141, 13, 12, 12, 12, 12, 12, 12, 12, 30,127,225,128,  0,  0,255,255,  0,  0,128,225,127, 30, 12, 12, 12, 12, 12, 12, 12, 13,141,207,239,255,127, 63, 15,  3,  0,  0,128,224,248,255, 63, 15,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,  0,  0,  0,  3,  7, 15, 62,124,248,240,224,192,128,  1,  3,  7, 15, 30, 28, 60, 56,120,112,112,224,224,225,231,254,248,255,255,248,254,231,225,224,224,112,112,120, 56, 60, 28, 30, 15,  7,  3,  1,128,192,224,240,248,124, 62, 15,  7,  3,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
-        0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  3,  7, 15, 14, 30, 28, 60, 56,120,112,112,112,224,224,224,224,224,224,224,224,224,224,224,224,224,224,224,224,112,112,112,120, 56, 60, 28, 30, 14, 15,  7,  3,  1,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0
-    };
-    oled_write_raw_P(kyria_logo, sizeof(kyria_logo));
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+	return OLED_ROTATION_180;
+}
+
+static void render_el_logo(void) {
+      oled_write_P(PSTR("[elasticrash ~(^_^)~]\n"), false);
+}
+
+static void render_layer_left(void) {
+    switch (get_highest_layer(layer_state)) {
+        case _QWERTY:
+            oled_write_P(PSTR("ESC Q W E R T\n"), false);
+            oled_write_P(PSTR("LR3 A S D F G\n"), false);
+            oled_write_P(PSTR("CTR Z X C V B\n"), false);
+            oled_write_P(PSTR("    ALT TAB LR2 SH SP\n"), false);
+            break;
+        case _RAISE:
+            oled_write_P(PSTR("__  1 2 3 4 5\n"), false);
+            oled_write_P(PSTR("__  _________\n"), false);
+            oled_write_P(PSTR("__  _________\n"), false);
+            oled_write_P(PSTR("    _________\n\n"), false);
+            break;
+        case _LOWER:
+            oled_write_P(PSTR("__  ! @ # $ PER\n"), false);
+            oled_write_P(PSTR("__  { } ( ) `\n"), false);
+            oled_write_P(PSTR("__  | ^ [ ] ~\n"), false);
+            oled_write_P(PSTR("    _____ ; = \n\n"), false);
+            break;
+        case _ADJUST:
+            oled_write_P(PSTR("RGB\n"), false);
+            oled_write_P(PSTR("F Keys\n"), false);
+            oled_write_P(PSTR("Arrows\n\n\n"), false);
+            break;
+        default:
+            oled_write_P(PSTR("Undefined\n"), false);
+    }
+}
+
+static void render_layer_right(void) {
+    switch (get_highest_layer(layer_state)) {
+        case _QWERTY:
+            oled_write_P(PSTR("    Y U I O P |\n"), false);
+            oled_write_P(PSTR("    H J K L ; '\n"), false);
+            oled_write_P(PSTR("    N M , . / EN\n"), false);
+            oled_write_P(PSTR("SP SH LR1 GU BS\n"), false);
+            break;
+        case _RAISE:
+            oled_write_P(PSTR("    6 7 8 9 0\n"), false);
+            oled_write_P(PSTR("    _________\n"), false);
+            oled_write_P(PSTR("    _________\n"), false);
+            oled_write_P(PSTR("_____________\n\n"), false);
+            break;
+        case _LOWER:
+            oled_write_P(PSTR("    ^ & * (  )  |\n"), false);
+            oled_write_P(PSTR("    + - = _ PER +\n"), false);
+            oled_write_P(PSTR("    & = , . SLS -\n"), false);
+            oled_write_P(PSTR("= ; _______\n\n"), false);
+            break;
+        case _ADJUST:
+            oled_write_P(PSTR("RGB\n"), false);
+            oled_write_P(PSTR("F Keys\n"), false);
+            oled_write_P(PSTR("Arrows\n\n\n"), false);
+            break;
+        default:
+            oled_write_P(PSTR("Undefined\n"), false);
+    }
 }
 
 void oled_task_user(void) {
-        render_kyria_logo();
+    if (is_keyboard_master()) {
+        render_el_logo();
+        render_layer_left();
+    } else {
+        render_el_logo();
+        render_layer_right();
+    }
 }
 #endif
 
--- keyboards/kyria/rev1/config.h
+++ keyboards/kyria/rev1/config.h
@@ -19,8 +19,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x0000
+#define VENDOR_ID       0x2937
+#define PRODUCT_ID      0x9827
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    splitkb
 #define PRODUCT         Kyria Keyboard
