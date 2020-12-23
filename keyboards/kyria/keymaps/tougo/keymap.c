3c058800e3fbcf2960f3751ec0faeb3eba0f723b
wlselastic <s.kouroupis@gmail.com>
2020-12-23 00:02:55 +0000

    revert to default logo

--- keyboards/kyria/keymaps/tougo/keymap.c
+++ keyboards/kyria/keymaps/tougo/keymap.c
@@ -141,76 +141,20 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 }
 
 static void render_el_logo(void) {
-      oled_write_P(PSTR("[elasticrash ~(^_^)~]\n"), false);
-}
+    oled_write_P(PSTR("[elasticrash ~(^_^)~]\n"), false);
+  static const char PROGMEM qmk_logo[] = {
+    0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
+    0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
+    0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0};
 
-static void render_layer_left(void) {
-    switch (get_highest_layer(layer_state)) {
-        case _QWERTY:
-            oled_write_P(PSTR("ESC Q W E R T\n"), false);
-            oled_write_P(PSTR("LR3 A S D F G\n"), false);
-            oled_write_P(PSTR("CTR Z X C V B\n"), false);
-            oled_write_P(PSTR("    ALT TAB LR2 SH SP\n"), false);
-            break;
-        case _RAISE:
-            oled_write_P(PSTR("__  1 2 3 4 5\n"), false);
-            oled_write_P(PSTR("__  _________\n"), false);
-            oled_write_P(PSTR("__  _________\n"), false);
-            oled_write_P(PSTR("    _________\n\n"), false);
-            break;
-        case _LOWER:
-            oled_write_P(PSTR("__  ! @ # $ PER\n"), false);
-            oled_write_P(PSTR("__  { } ( ) `\n"), false);
-            oled_write_P(PSTR("__  | ^ [ ] ~\n"), false);
-            oled_write_P(PSTR("    _____ ; = \n\n"), false);
-            break;
-        case _ADJUST:
-            oled_write_P(PSTR("RGB\n"), false);
-            oled_write_P(PSTR("F Keys\n"), false);
-            oled_write_P(PSTR("Arrows\n\n\n"), false);
-            break;
-        default:
-            oled_write_P(PSTR("Undefined\n"), false);
-    }
-}
-
-static void render_layer_right(void) {
-    switch (get_highest_layer(layer_state)) {
-        case _QWERTY:
-            oled_write_P(PSTR("    Y U I O P |\n"), false);
-            oled_write_P(PSTR("    H J K L ; '\n"), false);
-            oled_write_P(PSTR("    N M , . / EN\n"), false);
-            oled_write_P(PSTR("SP SH LR1 GU BS\n"), false);
-            break;
-        case _RAISE:
-            oled_write_P(PSTR("    6 7 8 9 0\n"), false);
-            oled_write_P(PSTR("    _________\n"), false);
-            oled_write_P(PSTR("    _________\n"), false);
-            oled_write_P(PSTR("_____________\n\n"), false);
-            break;
-        case _LOWER:
-            oled_write_P(PSTR("    ^ & * (  )  |\n"), false);
-            oled_write_P(PSTR("    + - = _ PER +\n"), false);
-            oled_write_P(PSTR("    & = , . SLS -\n"), false);
-            oled_write_P(PSTR("= ; _______\n\n"), false);
-            break;
-        case _ADJUST:
-            oled_write_P(PSTR("RGB\n"), false);
-            oled_write_P(PSTR("F Keys\n"), false);
-            oled_write_P(PSTR("Arrows\n\n\n"), false);
-            break;
-        default:
-            oled_write_P(PSTR("Undefined\n"), false);
-    }
+  oled_write_P(qmk_logo, false);
 }
 
 void oled_task_user(void) {
     if (is_keyboard_master()) {
         render_el_logo();
-        render_layer_left();
     } else {
         render_el_logo();
-        render_layer_right();
     }
 }
 #endif
